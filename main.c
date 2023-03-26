e4296c7cb2799df4ec3551bf31bfddc0a6be68a8
Ambiguousrelic <82973790+Ambiguousrelic@users.noreply.github.com>
2023-03-26 18:41:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,22 @@ Pamela Franco ci 5346389
 #include <stdio.h>
 #include <math.h>
 
+void opEntero();
+void opReal();
+void opFraccion();
+
+
 int ingresar();
+
+
+void opPotencia();
+void opSen();
+void opCos();
+void opTan();
+void opLog();
+void opCeil();
+void opFloor();
+void opRaizcuadrada();
 
 int main(){
 	
@@ -69,8 +84,61 @@ void opEntero(){
 }
 
 void opReal(){
-	printf("operacion real");
+	int opcionreal=0;
+	printf("\nOperaciones con numeros reales\n 1.Raiz Cuadrada  2. Potencia  3. Seno  4.Cos  5.Tan  6.Log  7.Ceil  8.Floor");
+  do
+  {	
+  		printf("\nCon que operacion desea trabajar:");
+  		scanf("%d",&opcionreal);
+  		getchar();
+  		switch (opcionreal) 
+		{
+			case 0:printf("\nFin del programa\n");
+        		break;
+    		case 1:opRaizcuadrada();
+        		break;
+    		case 2:opPotencia();
+    			break;
+    		case 3:opSen();
+    			break;
+    		case 4:opCos();
+    			break;
+    		case 5:opTan();
+    			break;
+    		case 6:opLog();
+    			break;
+    		case 7:opCeil();
+    			break;
+    		case 8:opFloor();
+    			break;
+    		default:printf("\nLas operaciones son entre 1, 2 , 3 , 4 , 5 , 6 , 7 y 8\n");
+		}
+	}while (opcionreal!=0);	
 }
+
+void opRaizcuadrada(){
+	int numero;
+	int r;
+	do
+	{	
+  		printf("\nIngrese un numero:");
+  		scanf("%d",&numero);
+  		getchar();
+	}while (numero<0);	
+	r=sqrt(numero);
+	printf("El raiz cuadrada de %d es: %d ", numero,r);
+}
+
+void opPotencia(){}
+void opSen(){}
+void opCos(){}
+void opTan(){}
+void opLog(){}
+void opCeil(){}
+void opFloor(){}
+
+
+
 
 void opFraccion(){
 	printf("operacion fraccion");
